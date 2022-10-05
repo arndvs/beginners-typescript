b96bd8e74c5732ff1e6450232e032984161e1b6f
Aaron Davis <aaron@ripemetrics.com>
2022-10-05 11:35:20 -0700

    solutions 9 & 10

--- src/09-promises.problem.ts
+++ src/09-promises.problem.ts
@@ -9,10 +9,30 @@ interface LukeSkywalker {
   gender: string;
 }
 
-export const fetchLukeSkywalker = async (): LukeSkywalker => {
+// solution 1
+export const fetchLukeSkywalker = async (): Promise<LukeSkywalker> => { // wrap in Promise on async function
   const data = await fetch("https://swapi.dev/api/people/1").then((res) => {
     return res.json();
   });
 
   return data;
+};
+
+//Solution 2 - a cast is a way to tell typescript that you know better than it does
+export const fetchLukeSkywalker2 = async () => {
+    const data = await fetch("https://swapi.dev/api/people/1").then((res) => {
+      return res.json();
+    });
+
+    return data as LukeSkywalker;
+  };
+
+
+// Solution 3
+export const fetchLukeSkywalker3 = async () => {
+    const data: LukeSkywalker = await fetch(
+      "https://swapi.dev/api/people/1"
+    ).then((res) => {
+      return res.json();
+    })
 };
