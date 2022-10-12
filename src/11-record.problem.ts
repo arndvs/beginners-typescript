e3482bfda459e0c0c31e6abc4430f247a41289d4
Aaron Davis <aaron@ripemetrics.com>
2022-10-12 11:30:13 -0700

    problem 11

--- src/11-record.problem.ts
+++ src/11-record.problem.ts
@@ -1,7 +1,15 @@
 import { expect, it } from "vitest";
 
 const createCache = () => {
-  const cache = {};
+  const cache: Record<string, string> = {};
+  // Record is a utlity type
+  // when you see index in a type error, it's usually referring to the key of an object
+
+// this would also work
+// const cache: {
+//     [id: string]: string;
+//     } = {}
+
 
   const add = (id: string, value: string) => {
     cache[id] = value;
@@ -34,3 +42,6 @@ it("Should remove values to the cache", () => {
 
   expect(cache.cache["123"]).toEqual(undefined);
 });
+
+
+ // rewrite the code above so there aee no errors in typescript
